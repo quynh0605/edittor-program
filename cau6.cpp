0195c7d7ddf35f436159165984c1cd66c69901dc
sv <nhuquynh1615@gmail.com>
2018-01-28 14:58:26 +0700

    nhu quynh

---
+++ cau6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cin>>a;
+  if a%4==0 && a%100!=0 || a%400==0
+      cout<<"nam nhuan\n";
+  else
+      cout<<"k nhuan\n";
+    
+return 0 ;
+}
